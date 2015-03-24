b2bdeda3cbab3cb94c8ef366796c8c1d904202a0
suwarjono <jono.daunbiru@gmail.com>
2015-03-24 10:17:17 +0700

    siang 24 maret

--- tracking/marine.js
+++ tracking/marine.js
@@ -8,6 +8,21 @@ Ext.onReady(function() {
 			},
 			params: {}
 	});
+	
+	
+	//var updateClock = function () {
+		//Ext.fly('jam').update(new Date().format('g:i:s A'));
+	//}
+	////var updateClock = function () {
+		////new Date().format('g:i:s A')
+	////};
+
+	//var runner = new Ext.util.TaskRunner();
+	//var task = runner.start({
+		 //run: updateClock,
+		 //interval: 1000
+	//});
+	
 
 	var time_t = 'dd-MM-YYYY hh:mm';
 	var content_t = '<html><head><title></title></head><body><div style="background:#E0E0E0; width: 100%; overflow: hidden; vertical-align: middle; "><div style="width: 65px; float: left;"> <img src="header.png"></div><div align=right style="width: 200px; float: right; font-size: 14px;">'+user_login+' <a href="http://monita.daunbiru.com:8999/tracking/logout.php">(logout)</a>  <br> '+time_t+' WITA</div><div style="font-size: 28px;">MONITA - Vessel Tracking System</div></div></body></html>';
@@ -38,14 +53,37 @@ Ext.onReady(function() {
 				xtype	: 'component',
 				id		: 'app-header-title',
 				html	: document.title,
-				flex 	: 1,
+				//flex 	: 1,
+				width	: '85%'
 				
 			},{
 				xtype	: 'label',
-				text	: 'halooo'
+				html	: '<div id="jam"></div>',
+				width	: '10%'
+			
+			
+			//*
+			
+			},{
 				
-				
-			}]
+				//id: 'clock',
+				xtype	: 'button',
+				html	: '<div id="">LOGOUT</div>',
+				width	: '5%'			
+			//*/
+			}],
+			
+			listeners : {
+                render :function() {                
+                    Ext.TaskManager.start({
+                        run : function() {
+                            Ext.fly('jam').update(Ext.Date.format(new Date(), 'F j, Y, g:i:s A'));  
+                         },
+                        //scope: me,
+                        interval : 1000
+                    });
+                }
+            }
 			
 			
 			
@@ -101,9 +139,9 @@ Ext.onReady(function() {
 	}
 	
 	
-	setInterval(function() {
-		update_tittle();
-	}, 10000);
+	//setInterval(function() {
+		//update_tittle();
+	//}, 10000);
 
     peta1 = Ext.getCmp('mymap');
 
