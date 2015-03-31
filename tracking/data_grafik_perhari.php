01cb1b9732637ae0883a56e15b17526d3d003f3b
suwarjono <jono.daunbiru@gmail.com>
2015-03-31 15:33:13 +0700

    grapik

--- tracking/data_grafik_perhari.php
+++ tracking/data_grafik_perhari.php
@@ -10,7 +10,7 @@ try {
 	$id = (isset($_GET['id']) && ($_GET['id'] <> '')) ? $_GET['id'] : '1';
 	$tgl = (isset($_GET['tgl']) && ($_GET['tgl'] <> '')) ? $_GET['tgl'] : date('Y-m-d') ;
 	//echo 'idkapal = '.$id. ' dan tanggal = '.$tgl.'<br>';
-	$query = 'SELECT date_format(tanggal, "%d-%b-%Y") as tanggal , sum(flow1_tot) as tot_fl1, sum(overflow1_tot) as tot_ovfl1, sum(flow2_tot) as tot_fl2, sum(overflow2_tot) as tot_ovfl2, sum(runhour1) as rh1, sum(runhour2) as rh2 
+	$query = 'SELECT sum(flow1_tot) as tot_fl1, sum(overflow1_tot) as tot_ovfl1, sum(flow2_tot) as tot_fl2, sum(overflow2_tot) as tot_ovfl2, sum(runhour1) as rh1, sum(runhour2) as rh2 
 				FROM data_perjam 
 				WHERE id_ship = '.$id.' and tanggal= "'.$tgl.'"';
 
