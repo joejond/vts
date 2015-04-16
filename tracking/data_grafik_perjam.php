fdb28b8c05d59df1143099b5ab1edb7685589ef6
marine_vts <marine@daunbiru.com>
2015-04-16 15:34:10 +0700

    lokal sp

--- tracking/data_grafik_perjam.php
+++ tracking/data_grafik_perjam.php
@@ -11,13 +11,13 @@ try {
 	$id = (isset($_GET['id']) && ($_GET['id'] <> '')) ? $_GET['id'] : '1';
 	$tgl = (isset($_GET['tgl']) && ($_GET['tgl'] <> '')) ? $_GET['tgl'] : date('Y-m-d') ;
 	//echo 'idkapal = '.$id. ' dan tanggal = '.$tgl.'<br>';
-	$query = 'SELECT date_format(tanggal, "%d-%b-%Y") as tanggal , jam, rpm1_avg as rpm1, prop1_avg as prop1, flow1_tot as flow1, overflow1_tot as overflow1, temp1_avg as temp1, press1_avg as press1, 
-					rpm2_avg as rpm2, prop2_avg as prop2, flow2_tot as flow2, overflow2_tot as overflow2, temp2_avg as temp2, press2_avg as press2, runhour1, runhour2 
-				FROM data_perjam 
-				WHERE id_ship = '.$id.' and tanggal= "'.$tgl.'"
-				ORDER BY tanggal desc, jam desc';
+	//$query = 'SELECT date_format(tanggal, "%d-%b-%Y") as tanggal , jam, rpm1_avg as rpm1, prop1_avg as prop1, flow1_tot as flow1, overflow1_tot as overflow1, temp1_avg as temp1, press1_avg as press1, 
+					//rpm2_avg as rpm2, prop2_avg as prop2, flow2_tot as flow2, overflow2_tot as overflow2, temp2_avg as temp2, press2_avg as press2, runhour1, runhour2 
+				//FROM data_perjam 
+				//WHERE id_ship = '.$id.' and tanggal= "'.$tgl.'"
+				//ORDER BY tanggal desc, jam desc';
 
-
+	$query = 'call kapal_perjam($id,$tgl)';
 	$sth = $db->prepare($query);
 	$sth->execute();
 	$result = $sth->fetchAll(PDO::FETCH_ASSOC);
