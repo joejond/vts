1660a3631293b4133b83087180dab185a8f97a70
suwarjono <jono.daunbiru@gmail.com>
2015-04-16 16:14:42 +0700

    grafik data perhari

--- tracking/data_grafik_perhari.php
+++ tracking/data_grafik_perhari.php
@@ -11,11 +11,12 @@ try {
 	$id = (isset($_GET['id']) && ($_GET['id'] <> '')) ? $_GET['id'] : '1';
 	$tgl = (isset($_GET['tgl']) && ($_GET['tgl'] <> '')) ? $_GET['tgl'] : date('Y-m-d') ;
 	//echo 'idkapal = '.$id. ' dan tanggal = '.$tgl.'<br>';
-	$query = 'SELECT sum(flow1_tot) as tot_fl1, sum(overflow1_tot) as tot_ovfl1, sum(flow2_tot) as tot_fl2, sum(overflow2_tot) as tot_ovfl2, sum(runhour1) as rh1, sum(runhour2) as rh2 
-				FROM data_perjam 
-				WHERE id_ship = '.$id.' and tanggal= "'.$tgl.'"';
+	//$query = 'SELECT sum(flow1_tot) as tot_fl1, sum(overflow1_tot) as tot_ovfl1, sum(flow2_tot) as tot_fl2, sum(overflow2_tot) as tot_ovfl2, sum(runhour1) as rh1, sum(runhour2) as rh2 
+				//FROM data_perjam 
+				//WHERE id_ship = '.$id.' and tanggal= "'.$tgl.'"';
 
-
+	$query = 'call kapal_perhari('.$id.',"'.$tgl.'")';
+	
 	$sth = $db->prepare($query);
 	$sth->execute();
 	$result = $sth->fetchAll(PDO::FETCH_ASSOC);
