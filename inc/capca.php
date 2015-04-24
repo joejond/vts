074dc8ff9e807d380101522427b52a9253946dff
suwarjono <jono.daunbiru@gmail.com>
2015-04-24 16:54:42 +0700

    ganti font

--- inc/capca.php
+++ inc/capca.php
@@ -19,7 +19,7 @@ $image_p = imagecreate($width, $height);
 $graybg = imagecolorallocate($image_p, 245, 245, 245);
 $textcolor = imagecolorallocate($image_p, 34, 34, 34);
 
-imagefttext($image_p, $font_size, -2, 15, 26, $textcolor, 
+imagefttext($image_p, $font_size, -2, 2, 26, $textcolor, 
 '../fonts/times_new_yorker.ttf', $ranStr);
 imagepng($image_p);
 //* */
