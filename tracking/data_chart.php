56cb1e5c081ff6ff3bdbf4afa0832ee91f60bd84
suwarjono <jono.daunbiru@gmail.com>
2015-05-01 10:47:42 +0800

    siangan

--- tracking/data_chart.php
+++ tracking/data_chart.php
@@ -5,13 +5,14 @@ include	'../inc/conn_db.php';
 //include	'../inc/conn_db_linode2.php';
 include	'../inc/cekSession.php';
 
-
+//echo date_default_timezone_get();
 try {
 	//balik
 	$id = (isset($_GET['id']) && ($_GET['id'] <> '')) ? $_GET['id'] : '1';
 	$tgl = (isset($_GET['tgl']) && ($_GET['tgl'] <> '')) ? $_GET['tgl'] : date('Y-m-d') ;
 	
 	//echo 'idkapal = '.$id. ' dan tanggal = '.$tgl.'<br>';
+	/*
 	for($i=0; $i<24;$i++){
 			$perjam[] = array(
 				'jam' =>  $i,'rpm1' => null,'rpm2' => null,'rpm3' => null,
@@ -25,61 +26,67 @@ try {
 	$sth->execute();
 	$result = $sth->fetchAll(PDO::FETCH_ASSOC);
 	
-	$hasil = array();
-	foreach ($result as $rr){
-		$hasil[] = array( 
-			'jam' => $rr['jam'],
-			'rpm1' => !isset ($rr['rpm1']) ? null : $rr['rpm1'],
-			'rpm2' => !isset ($rr['rpm2']) ? null : $rr['rpm2'],
-			'rpm3' => !isset ($rr['rpm3']) ? null : $rr['rpm3'],  
-			'fuel1' => !isset ($rr['inflow1']) && !isset ($rr['outflow1']) ? null : (($rr['inflow1']) - ($rr['outflow1'])),
-			'fuel2' => !isset ($rr['inflow2']) && !isset ($rr['outflow2']) ? null : (($rr['inflow2']) - ($rr['outflow2'])),
-			'fuel3' => !isset ($rr['inflow3']) && !isset ($rr['outflow3']) ? null : (($rr['inflow3']) - ($rr['outflow3'])),
-			'rh1' => !isset ($rr['runhour1']) ? null : $rr['runhour1'],
-			'rh2' => !isset ($rr['runhour2']) ? null : $rr['runhour2'],
-			'rh3' => !isset ($rr['runhour3']) ? null : $rr['runhour3']
+	print_r($result);
+	
+	//$hasil = array();
+	//foreach ($result as $rr){
+		//$hasil[] = array( 
+			//'jam' => $rr['jam'],
+			//'rpm1' => !isset ($rr['rpm1']) ? '' : $rr['rpm1'],
+			//'rpm2' => !isset ($rr['rpm2']) ? null : $rr['rpm2'],
+			//'rpm3' => !isset ($rr['rpm3']) ? null : $rr['rpm3'],  
+			//'fuel1' => !isset ($rr['inflow1']) && !isset ($rr['outflow1']) ? null : (($rr['inflow1']) - ($rr['outflow1'])),
+			//'fuel2' => !isset ($rr['inflow2']) && !isset ($rr['outflow2']) ? null : (($rr['inflow2']) - ($rr['outflow2'])),
+			//'fuel3' => !isset ($rr['inflow3']) && !isset ($rr['outflow3']) ? null : (($rr['inflow3']) - ($rr['outflow3'])),
+			//'rh1' => !isset ($rr['runhour1']) ? null : $rr['runhour1'],
+			//'rh2' => !isset ($rr['runhour2']) ? null : $rr['runhour2'],
+			//'rh3' => !isset ($rr['runhour3']) ? null : $rr['runhour3']
 			
-		);
-	}
+		//);
+	//}
 	
 	$grafik = array_replace($perjam,$hasil);
 	
+	//*/
+	//*
 	
-	
-	//for($i=0; $i<24;$i++){
-		////$query = 'SELECT rpm1_avg, rpm2_avg, flow1_tot, overflow1_tot, flow2_tot, overflow2_tot, runhour1, runhour2
-					////FROM data_perjam 
-			////where id_ship = '.$id.' and tanggal = "'.$tgl.'" and jam = "'.$i.'"';
+	for($i=0; $i<24;$i++){
+		//$query = 'SELECT rpm1_avg, rpm2_avg, flow1_tot, overflow1_tot, flow2_tot, overflow2_tot, runhour1, runhour2
+					//FROM data_perjam 
+			//where id_ship = '.$id.' and tanggal = "'.$tgl.'" and jam = "'.$i.'"';
 		
-		//$query = 'call kapal_perjam_chart('.$id.', "'.$tgl.'",'.$i.')';
+		$query = 'call kapal_perjam_chart('.$id.', "'.$tgl.'",'.$i.')';
 		
-		//$sth = $db->prepare($query);
-		//$sth->execute();
+		$sth = $db->prepare($query);
+		$sth->execute();
 		
-		//$r = $sth->fetch();
+		$r = $sth->fetch();
 		
-		////echo 'jame ke- '.$i.'<br>';
-		////echo 'rpm1 = '.$r['rpm1'].'<br>';
-		////while ($r = $sth->fetch()){
+		//echo 'jame ke- '.$i.'<br>';
+		//echo 'rpm1 = '.$r['rpm1'].'<br>';
+		//while ($r = $sth->fetch()){
 		
-		//$data_chart [] = array(
-			//'jam' =>  (strlen($i) ==1 )? '0'.$i : $i,
-			//'rpm1' => ($r['rpm1'] == '' ) ? null : $r['rpm1'],
-			//'rpm2' => ($r['rpm2'] == '' ) ? null : $r['rpm2'],
-			//'fuel1' => ($r['flow1'] == '' ) ? null : $r['flow1'] - $r['overflow1'],
-			//'fuel2' => ($r['flow2'] == '' ) ? null : $r['flow2'] - $r['overflow2'],
-			//'rh1' => ($r['runhour1'] == '' ) ? null : $r['runhour1'],
-			//'rh2' => ($r['runhour1'] == '' ) ? null : $r['runhour2']
-		//);
+		$data_chart [] = array(
+			'jam' =>  (strlen($i) ==1 )? '0'.$i : $i,
+			'rpm1' => ($r['rpm1'] == '' ) ? null : $r['rpm1'],
+			'rpm2' => ($r['rpm2'] == '' ) ? null : $r['rpm2'],
+			//'rpm3' => ($r['rpm3'] == '' ) ? null : $r['rpm3'],
+			'fuel1' => ($r['flow1'] == '' ) ? null : $r['flow1'] - $r['overflow1'],
+			'fuel2' => ($r['flow2'] == '' ) ? null : $r['flow2'] - $r['overflow2'],
+			//'fuel3' => ($r['flow3'] == '' ) ? null : $r['flow3'] - $r['overflow3'],
+			'rh1' => ($r['runhour1'] == '' ) ? null : $r['runhour1'],
+			'rh2' => ($r['runhour2'] == '' ) ? null : $r['runhour2'],
+			//'rh3' => ($r['runhour3'] == '' ) ? null : $r['runhour3']
+		);
 		
 		
-	//}
-	
+	}
+	//*/
 	//print_r ($data_chart);
 	
 	$jsonResult = array(
         'success' => true,
-        'chart' => $grafik
+        'chart' => $data_chart
     );
 } catch(Exception $e) {
     $jsonResult = array(
