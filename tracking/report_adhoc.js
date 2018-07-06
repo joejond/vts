5830423469271df5127cd16316120037ae910e68
zenaki <zenaki.zen.aku@gmail.com>
2018-07-06 17:13:39 +0700

    Perbaikan Month Picker

--- tracking/report_adhoc.js
+++ tracking/report_adhoc.js
@@ -592,7 +592,7 @@ var store_adhoc_kapal = Ext.create('Ext.data.Store', {
     proxy: {
         type: 'ajax',
         // url:'http://project.daunbiru.com:1336/get_data_adhoc',
-        url:'getAPI()+/get_data_adhoc',
+        url:getAPI()+'/get_data_adhoc',
         timeout: 40000,
         //url: 'http://project.daunbiru.com:1337/get_data_bima?id=8&user_id=4&tz=%2B07&tgl=2018-02-01',
         method: 'GET',
