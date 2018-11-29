edfb122b0ded85eb6540a16ba1101b891f5f390b
zenaki <zenaki.zen.aku@gmail.com>
2018-11-29 15:07:12 +0700

    Perbaikan BUG

--- tracking/report_adhoc.js
+++ tracking/report_adhoc.js
@@ -1222,7 +1222,7 @@ var store_fuel_sonding = Ext.create('Ext.data.Store', {
 		proxy: {
 				type: 'ajax',
 				// url:getAPI()+'/pelindo/custom_input?titik_ukur_id=12005',
-        url:getAPI()+'/pelindo/custom_input?titik_ukur_tipe_id=41&aset_id='+Ext.getCmp('cb_vessel_adhoc').getValue(),
+        url:getAPI()+'/pelindo/custom_input?titik_ukur_tipe_id=41&aset_id='+id_vessel_adhoc,
 				method: 'GET',
 				// params: {titik_ukur_id:12005},
 				// reader: {
@@ -1400,7 +1400,7 @@ var store_work_order = Ext.create('Ext.data.Store', {
 				// url:'http://10.10.10.11:1336/pelindo/custom_input?titik_ukur_id=11033',
 				// url:'http://192.168.1.17:1337/pelindo/custom_input?titik_ukur_id=11033',
 				// url:getAPI()+'/pelindo/work_order?titik_ukur_id=11106',
-        url:getAPI()+'/pelindo/work_order?titik_ukur_tipe_id=106&aset_id='+Ext.getCmp('cb_vessel_adhoc').getValue(),
+        url:getAPI()+'/pelindo/work_order?titik_ukur_tipe_id=106&aset_id='+id_vessel_adhoc,
 				method: 'GET',
 				// params: {titik_ukur_id:12005},
 				// reader: {
